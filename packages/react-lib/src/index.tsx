18e5ebc0a7beaf39740089ce9aba350fa429f4ac
Luis <takahashihideki408@gmail.com>
2020-07-28 17:53:55 -0300

    Added initial react-lib package

---
+++ packages/react-lib/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  return (
+    <div>
+      <h1>Cleber da silva</h1>
+      Adding a new component stuff
+      <div>the snozzberries taste like snozzberries</div>
+    </div>
+  );
+};
